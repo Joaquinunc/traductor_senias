943574e4f61047ce915495b30c58fb420373ac2b
Joaquin Ferreyra <joaquin.ignacio.ferreyra@mi.unc.edu.ar>
2026-03-09 01:57:35 -0300

    FIX optimizacion vectorial de gestos, DEL archivo sin uso

--- five_nums.py
+++ five_nums.py
@@ -2,55 +2,43 @@ import mediapipe as mp
 from mediapipe.framework.formats import landmark_pb2
 
 def gesture_classifier(handlines, mp_hands):
-    ifinger_top = handlines.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
-    thumb_y = handlines.landmark[mp_hands.HandLandmark.THUMB_TIP].y 
-    thumb_ip_y = handlines.landmark[mp_hands.HandLandmark.THUMB_IP].y
-    mfinger_top = handlines.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y
-    rfinger_top = handlines.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y
-    pfinger_top = handlines.landmark[mp_hands.HandLandmark.PINKY_TIP].y
-
+    # arreglo que tiene los vectores tridimensionales de las puntas de los dedos
+    finger_tips = [
+        mp_hands.HandLandmark.THUMB_TIP,
+        mp_hands.HandLandmark.INDEX_FINGER_TIP,
+        mp_hands.HandLandmark.MIDDLE_FINGER_TIP,
+        mp_hands.HandLandmark.RING_FINGER_TIP,
+        mp_hands.HandLandmark.PINKY_TIP
+    ]
+    # eje y de la base de la falange del dedo medio
     mfinger_bot = handlines.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y
-    thumb_x = handlines.landmark[mp_hands.HandLandmark.THUMB_TIP].x
-    ifinger_x = handlines.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x 
-    return ifinger_top, thumb_y, mfinger_top, rfinger_top, pfinger_top, thumb_x, ifinger_x, mfinger_bot, thumb_ip_y 
+
+    # vector de 5 entradas, una para cada punta de un dedo
+    vectorial_finger = [] 
+
+    #recorremos el arreglo agregando 0s/1s segun la posicion sea abajo o arriba de mfinger_bot
+    for tip in finger_tips:
+        if handlines.landmark[tip].y < mfinger_bot:
+            vectorial_finger.append(1)
+        else:
+            vectorial_finger.append(0)
+   
+    return vectorial_finger
 
 def gesture_processor(handlines: landmark_pb2.NormalizedLandmarkList, mp_hands):
-
-     #deteccion de posiciones de dedos especificas
-    ifinger_top,thumb_y, mfinger_top, rfinger_top, pfinger_top, thumb_x, ifinger_x, mfinger_bot, thumb_ip_y = gesture_classifier(handlines, mp_hands)
-    # matcheo de patrones
-    # caso 1: ☝️
-    isOne = ((ifinger_top < mfinger_bot)
-             and (mfinger_top > mfinger_bot) and(rfinger_top > mfinger_bot) and (pfinger_top> mfinger_bot)
-             and (thumb_y < mfinger_top) and (thumb_y < pfinger_top) and (thumb_y < rfinger_top))
-    #✌️
-    isTwo = ((ifinger_top < mfinger_bot)
-             and (mfinger_top < mfinger_bot) and(rfinger_top > mfinger_bot) and (pfinger_top> mfinger_bot)
-             and (thumb_y < pfinger_top) and (thumb_y < rfinger_top))
-    #tres dedos hacia arriba
-    isThree = ((ifinger_top < mfinger_bot)
-             and (mfinger_top < mfinger_bot) and(rfinger_top < mfinger_bot) and (pfinger_top> mfinger_bot)
-             and (thumb_y < pfinger_top))
-    #cuatro dedos hacia arriba
-    isFour = ((ifinger_top < mfinger_bot)
-             and (mfinger_top < mfinger_bot) and(rfinger_top < mfinger_bot) and (pfinger_top< mfinger_bot)
-             and (thumb_y > mfinger_bot))
-    #✋
-    isFive = ((ifinger_top < mfinger_bot)
-             and (mfinger_top < mfinger_bot) and(rfinger_top < mfinger_bot) and (pfinger_top< mfinger_bot)
-    )
-
-    if isOne:
-        hand_gesture = 'Uno'
-    elif isTwo:
-        hand_gesture = 'Dos' 
-    elif isThree:
-        hand_gesture = 'Tres'
-    elif isFour:
-        hand_gesture = 'Cuatro'
-    elif isFive:
-        hand_gesture = 'Cinco'
-    else:
-        hand_gesture = ''
-
-    return hand_gesture 
+    #obtenemos el vector de posiciones para un gesto
+    states = gesture_classifier(handlines, mp_hands)
+    #print(f"states: {states}")
+    # diccionario con tipos de gestos para cada posicion de manos
+    gestos = {
+        (0, 1, 0, 0, 0): "Uno",
+        (0, 1, 1, 0, 0): "Dos",
+        (0, 1, 1, 1, 0): "Tres",
+        (0, 1, 1, 1, 1): "Cuatro",
+        (1, 1, 1, 1, 1): "Cinco",
+    }
+    
+    # obtenemos el valor en base al vector (en formato legible)
+    hand_gesture = gestos.get(tuple(states), "")
+    #print(f"hand_gesture: {hand_gesture}")
+    return hand_gesture
