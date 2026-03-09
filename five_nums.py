b7fc2e9f75e5f619f3b4bfaa7294866b6336cbaf
Joaquin Ferreyra <joaquin.ignacio.ferreyra@mi.unc.edu.ar>
2026-03-08 23:39:43 -0300

    FIX gestos contador

--- five_nums.py
+++ five_nums.py
@@ -4,6 +4,7 @@ from mediapipe.framework.formats import landmark_pb2
 def gesture_classifier(handlines, mp_hands):
     ifinger_top = handlines.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
     thumb_y = handlines.landmark[mp_hands.HandLandmark.THUMB_TIP].y 
+    thumb_ip_y = handlines.landmark[mp_hands.HandLandmark.THUMB_IP].y
     mfinger_top = handlines.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y
     rfinger_top = handlines.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y
     pfinger_top = handlines.landmark[mp_hands.HandLandmark.PINKY_TIP].y
@@ -11,25 +12,25 @@ def gesture_classifier(handlines, mp_hands):
     mfinger_bot = handlines.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y
     thumb_x = handlines.landmark[mp_hands.HandLandmark.THUMB_TIP].x
     ifinger_x = handlines.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x 
-    return ifinger_top, thumb_y, mfinger_top, rfinger_top, pfinger_top, thumb_x, ifinger_x, mfinger_bot 
+    return ifinger_top, thumb_y, mfinger_top, rfinger_top, pfinger_top, thumb_x, ifinger_x, mfinger_bot, thumb_ip_y 
 
 def gesture_processor(handlines: landmark_pb2.NormalizedLandmarkList, mp_hands):
 
      #deteccion de posiciones de dedos especificas
-    ifinger_top,thumb_y, mfinger_top, rfinger_top, pfinger_top, thumb_x, ifinger_x, mfinger_bot = gesture_classifier(handlines, mp_hands)
+    ifinger_top,thumb_y, mfinger_top, rfinger_top, pfinger_top, thumb_x, ifinger_x, mfinger_bot, thumb_ip_y = gesture_classifier(handlines, mp_hands)
     # matcheo de patrones
     # caso 1: ☝️
     isOne = ((ifinger_top < mfinger_bot)
              and (mfinger_top > mfinger_bot) and(rfinger_top > mfinger_bot) and (pfinger_top> mfinger_bot)
-             and (thumb_y > mfinger_bot))
+             and (thumb_y < mfinger_top) and (thumb_y < pfinger_top) and (thumb_y < rfinger_top))
     #✌️
     isTwo = ((ifinger_top < mfinger_bot)
              and (mfinger_top < mfinger_bot) and(rfinger_top > mfinger_bot) and (pfinger_top> mfinger_bot)
-             and (thumb_y > mfinger_bot))
+             and (thumb_y < pfinger_top) and (thumb_y < rfinger_top))
     #tres dedos hacia arriba
     isThree = ((ifinger_top < mfinger_bot)
              and (mfinger_top < mfinger_bot) and(rfinger_top < mfinger_bot) and (pfinger_top> mfinger_bot)
-             and (thumb_y > mfinger_bot))
+             and (thumb_y < pfinger_top))
     #cuatro dedos hacia arriba
     isFour = ((ifinger_top < mfinger_bot)
              and (mfinger_top < mfinger_bot) and(rfinger_top < mfinger_bot) and (pfinger_top< mfinger_bot)
